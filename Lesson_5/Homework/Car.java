f9aea4b60a60f77fa8600ed34a7e82365a955c21
ngnevan <n_gnevanov@mail.ru>
2019-10-31 20:43:25 +0300

    Сделал домашнее задание номер 5

---
+++ Lesson_5/Homework/Car.java
@@ -0,0 +1,54 @@
+package Lesson_5.Homework;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+
+public class Car implements Runnable {
+    private static int CARS_COUNT;
+    static {
+        CARS_COUNT = 0;
+    }
+    private Race race;
+    private int speed;
+    private String name;
+    private CyclicBarrier cb;
+    private CountDownLatch cdl;
+    public String getName() {
+        return name;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public Car(Race race, int speed, CyclicBarrier cb, CountDownLatch cdl) {
+        this.race = race;
+        this.speed = speed;
+        CARS_COUNT++;
+        this.name = "Участник #" + CARS_COUNT;
+        this.cb = cb;
+        this.cdl = cdl;
+    }
+    @Override
+    public void run() {
+        try {
+            System.out.println(this.name + " готовится");
+            Thread.sleep(500 + (int)(Math.random() * 800));
+            System.out.println(this.name + " готов");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            cb.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+        System.out.println(this.name + " запустился");
+        for (int i = 0; i < race.getStages().size(); i++) {
+            race.getStages().get(i).go(this);
+        }
+        cdl.countDown();
+    }
+
+}
